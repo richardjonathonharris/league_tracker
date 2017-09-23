5e5d05135cb47566fcbc1b14f9486a7f4887a8eb
Richard Harris <richard.jonathon.harris@gmail.com>
2017-09-23 17:49:07 +0000

    Missed quote

--- league_tracker/management/commands/createsu.py
+++ league_tracker/management/commands/createsu.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@admin.com", admin")
+        User.objects.create_superuser("admin", "admin@admin.com", "admin")
         self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
